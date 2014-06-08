5a2bd2e36a6facd7105c67305eb66d6d4a206d43
Remy Sharp <remy@remysharp.com>
2014-06-08 09:42:38 +0100

    URL access

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ function rnd() {
 }
 
 http.createServer(function (req, res) {
-  var i = rnd();
-  var method = trouble[methods[i]];
+  // get the url without a query string
+  var url = req.url.replace(/\?.*/, '');
 
-  console.log('method %s [%s]', method, i);
+  if (url === '/') {
+    var i = rnd();
+    var method = trouble[methods[i]];
 
-  res.writeHead(200, {'content-type': 'text/html'});
-  method(res);
+    console.log('method %s [%s]', method, i);
+
+    res.writeHead(200, {'content-type': 'text/html'});
+    method(res);
+  } else if (trouble[url.slice(1)]) {
+    res.writeHead(200, {'content-type': 'text/html'});
+    trouble[url.slice(1)](res);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 }).listen(process.env.PORT || 8000);
--- trouble.js
+++ trouble.js
@@ -1,26 +1,43 @@
 'use strict';
+
+function Message(s) {
+  this.value = s;
+}
+
+Message.prototype.toString = function () {
+  return this.value + '<!-- padding ' + new Array(2048).join(' ') + '-->';
+};
+
 module.exports = {
   one: function one(res) {
     var ctr = 0;
+    var m;
+
     while (true) {
-      res.write(ctr++ + '');
+      ctr++;
+      m = new Message('.\n');
+      res.write(m.toString());
     }
     res.end();
   },
   two: function two(res) {
     var ctr = 100;
     setInterval(function () {
-      res.write(ctr-- + '');
+      ctr--;
+      var m = new Message((100 - ctr) + '\n');
+      res.write(m.toString());
     }, 200);
 
     setTimeout(function () {
       res.end();
-    }, 200 * 100 + 20);
+    }, (200 * 100) + 201);
   },
   three: function three(res) {
     var ctr = 100;
+    var m;
     while (ctr--) {
-      res.write(ctr+'');
+      m = new Message('.\n');
+      res.write(m.toString());
     }
     res.end();
   },
